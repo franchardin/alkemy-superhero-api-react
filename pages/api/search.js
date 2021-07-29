576169141f15b47a22cf634a85e166ff3a91a58b
jpromanonet <programming@jpromano.net>
2021-07-28 21:38:27 -0300

    Cache for search api file

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,5 +1,10 @@
+import post from '../scripts/cache'
+
 export default(req, res) => {
+    const results = req.query.q ?
+        posts.filter(post => post.name.toLowerCase().includes(req.query.q)): []
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({results: []}))
+    res.end(JSON.stringify({results}))
 }
+
