ac739b946e2ed94e7863d6027a3fa919900b8034
jpromanonet <programming@jpromano.net>
2021-07-28 21:28:14 -0300

    Cache for search api file

---
+++ pages/scripts/cache.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+
+function getPosts(){
+    const postsDirectory = path.join(process.cwd(), 'posts')
+    const fileNames = fs.readdirSync(postsDirectory)
+    const posts = fileNames.map(fileName => {
+        const id = fileName.replace(/\.md$/,'')
+        const fullPath = path.join(postsDirectory, fileName)
+        const fileContents = fs.readFileSync(fullPath, 'utf8')
+        const matterResult = matter(fileContents)
+        return {
+            id,
+            name
+        }
+    })
+    return JSON.stringify(posts)
+}
+
+const fileContents = 
